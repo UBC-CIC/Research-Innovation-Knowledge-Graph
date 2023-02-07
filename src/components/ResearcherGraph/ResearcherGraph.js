fbd6a892275f8a4661ab7619fb3433a07fccfc6c
stefansson30952 <stefansson30952@gmail.com>
2023-02-06 16:17:50 -0800

    added researcher node and edges connection to backend

--- src/components/ResearcherGraph/ResearcherGraph.js
+++ src/components/ResearcherGraph/ResearcherGraph.js
@@ -10,16 +10,93 @@ import { Container, Card, Grid, CardContent, Typography } from "@mui/material";
 import "./ResearcherGraph.css";
 import GraphDefinition from "./helpers/GraphDefinition";
 import GraphEvents from "./helpers/GraphEvents";
+import Graph from "graphology";
+import {random } from 'graphology-layout';
+import forceAtlas2 from 'graphology-layout-forceatlas2';
+
+import Amplify from "@aws-amplify/core";
+import { Auth } from "@aws-amplify/auth";
+import awsmobile from "../../aws-exports";
+import { API } from "aws-amplify";
+
+import {
+  getResearchers,
+  getEdges,
+} from "../../graphql/queries";
+
+Amplify.configure(awsmobile);
+Auth.configure(awsmobile);
+
 const ResearcherGraph = () => {
   const [graph, setGraph] = useState(null);
   const [selectedNode, setSelectedNode] = useState(null);
   const [selectedEdge, setSelectedEdge] = useState(null);
 
+  const [researcherNodes, setResearcherNodes] = useState([]);
+  const [graphEdges, setGraphEdges] = useState([]);
+
   //can use setSelectNode to set the node selected during search
 
   useEffect(() => {
-    setGraph(GraphDefinition());
-  }, []);
+    const jsonGraph = {
+      attributes:{},
+      nodes: researcherNodes,
+      edges: graphEdges,
+    }
+
+    const graph = Graph.from(jsonGraph)
+    random.assign(graph); //assigns each node a random x,y value between [0,1]
+    forceAtlas2.assign(graph, {iterations: 100}); //assigns nodes x,y values with force directed
+    setGraph(graph)
+
+  }, [researcherNodes, graphEdges])
+
+  useEffect(() => {
+    getResearcherNodes();
+    getResearcherEdges();
+  }, [])
+
+  const getResearcherNodes = async () => {
+    const researchers = await API.graphql({
+      query: getResearchers,
+    });
+
+    let researcherList = researchers.data.getResearchers;
+
+    let ResearcherNodes = [];
+    let toStopDups = [];
+
+    for(let i = 0; i<researcherList.length; i++) {
+      let fullName = researcherList[i].firstName + ' ' + researcherList[i].lastName
+      let key = researcherList[i].id
+      //Key should probably be an ID
+      if(!toStopDups.includes(researcherList[i].id)) {
+        ResearcherNodes.push({key: researcherList[i].id, attributes: {label: fullName, size:10}})
+        toStopDups.push(researcherList[i].id)
+      }
+    }
+
+    setResearcherNodes(ResearcherNodes);
+  }
+
+  const getResearcherEdges = async () => {
+    const edgesResult = await API.graphql({
+      query: getEdges,
+    });
+
+    let edges = edgesResult.data.getEdges;
+
+    let researcherEdges = [];
+    
+    for(let i = 0; i<edges.length; i++) {
+      researcherEdges.push({key:edges[i].key, source: edges[i].source, target: edges[i].target, 
+        undirected:true, attributes:{size: 1, color:"red"}})//attributes:{size:edges[i].size, color:"red"}})
+    }
+
+
+    console.log(researcherEdges)
+    setGraphEdges(researcherEdges)
+  }
 
   return (
     <div className="Researcher-Graph">
