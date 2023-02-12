247c683cddd6012c90c91b08a84770e618dc2cd5
Wren Liang <wren.liang@gmail.com>
2023-02-11 18:50:44 -0800

    colors backup

---
+++ scripts/colors.py
@@ -0,0 +1,23 @@
+colorObject = {
+    "Faculty of Applied Science": "#A84D52", # Redwood
+    "Faculty of Science": "#2E5090", # YInMn Blue
+    "The Sauder School of Business": "#88BD56", # Pistachio
+    "Faculty of Dentistry": "#00B8A2", # Keppel
+    "Faculty of Arts": "#634581", # Ultra Violet
+    "Faculty of Forestry": "#82470D", # Chocolate
+    "Faculty of Land and Food Systems": "#ADA871", # Sage
+    "Faculty of Education": "#025033", # Castleton green
+    "Faculty of Pharmaceutical Sciences": "#DBB2D1", # Pink lavender
+    
+    "Peter A. Allard School of Law": "#F7CB78", # Jasmine
+    "Faculty of Medicine": "#40AFD1", # Pacific cyan
+
+    "VP Academic and Provost": "#989C9C", # other shade of grey
+    "VP Research and Innovation": "#989C9C",
+
+    "UBCO - Barber - Faculty of Science": "#A3A9AA", # some sort of grey (Cadet gray)
+    "UBCO - Barber - Faculty of Arts and Social Sciences": "#A3A9AA",
+    "UBCO - Faculty of Creative and Critical Studies": "#A3A9AA",
+    "UBCO - Faculty of Health and Social Development": "#A3A9AA",
+    "UBCO - Faculty of Management": "#A3A9AA",
+}
