625e668d7604d7ae2968006c3ae0c6895ece2bc1
stefansson30952 <stefansson30952@gmail.com>
2023-02-12 17:30:14 -0800

    moved filter button, added colors to filters and updated search bar

--- src/components/FacultyFiltersDialog.js
+++ src/components/FacultyFiltersDialog.js
@@ -10,6 +10,7 @@ import {
   Button,
 } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 
 const FacultyFiltersDialog = ({
   open,
@@ -17,8 +18,33 @@ const FacultyFiltersDialog = ({
   allFaculties,
   selectedFaculties,
   handleCheckFaculty,
+  applyFilters,
 }) => {
   const renderAllFaculties = () => {
+    let colorObject = {
+      "Faculty of Applied Science": "#A84D52", // Redwood
+      "Faculty of Science": "#2E5090", // YInMn Blue
+      "The Sauder School of Business": "#88BD56", // Pistachio
+      "Faculty of Dentistry": "#00B8A2", // Keppel
+      "Faculty of Arts": "#634581", // Ultra Violet
+      "Faculty of Forestry": "#82470D", // Chocolate
+      "Faculty of Land and Food Systems": "#ADA871", // Sage
+      "Faculty of Education": "#025033", // Castleton green
+      "Faculty of Pharmaceutical Sciences": "#DBB2D1", // Pink lavender
+      
+      "Peter A. Allard School of Law": "#F7CB78", // Jasmine
+      "Faculty of Medicine": "#40AFD1", // Pacific cyan
+  
+      "VP Academic and Provost": "#989C9C", // other shade of grey
+      "VP Research and Innovation": "#989C9C",
+  
+      "UBCO - Barber - Faculty of Science": "#A3A9AA", // some sort of grey (Cadet gray)
+      "UBCO - Barber - Faculty of Arts and Social Sciences": "#A3A9AA",
+      "UBCO - Faculty of Creative and Critical Studies": "#A3A9AA",
+      "UBCO - Faculty of Health and Social Development": "#A3A9AA",
+      "UBCO - Faculty of Management": "#A3A9AA",
+  }
+
     return (
       allFaculties && (
         <Box
@@ -31,13 +57,15 @@ const FacultyFiltersDialog = ({
         >
           <FormGroup sx={{ display: "flex", flexDirection: "row" }}>
             {allFaculties.map((faculty, index) => (
+              <div>
               <FormControlLabel
                 key={index}
                 control={<Checkbox />}
-                label={<Typography variant="body2">{faculty}</Typography>}
+                label={<Typography variant="body2"><IconButton><FiberManualRecordIcon style={{ color: colorObject[faculty] }} /></IconButton>{faculty}</Typography>}
                 checked={selectedFaculties.includes(faculty)}
                 onChange={(e) => handleCheckFaculty(e, faculty)}
               />
+              </div>
             ))}
           </FormGroup>
         </Box>
@@ -60,7 +88,7 @@ const FacultyFiltersDialog = ({
         <Button
           variant="outlined"
           sx={{ color: "#0055b7" }}
-          onClick={() => handleClose()}
+          onClick={() => {handleClose(); applyFilters();}}
         >
           Apply Filters
         </Button>
