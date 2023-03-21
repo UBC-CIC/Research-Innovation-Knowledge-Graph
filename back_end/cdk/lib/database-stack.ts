590e63c4f8c2479e424c291a2808e24ff14e7255
Lance Holland <lanceholland01@gmail.com>
2023-03-21 15:29:33 -0700

    lambda, vpc, and database stacks finished

---
+++ back_end/cdk/lib/database-stack.ts
@@ -0,0 +1,56 @@
+import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
+import { aws_rds as rds } from 'aws-cdk-lib';
+import { VpcStack } from './vpc-stack';
+import * as logs from 'aws-cdk-lib/aws-logs'
+import * as sm from 'aws-cdk-lib/aws-secretsmanager'
+
+export class DatabaseStack extends Stack {
+    public readonly dbInstance: rds.DatabaseInstance;
+    public readonly secretPath: string;
+
+    constructor(scope: Construct, id: string, vpcStack: VpcStack, props?: StackProps) {
+      super(scope, id, props);
+
+    this.secretPath = 'knowledgeGraph/credentials/dbCredentials';
+
+    // Database secret with customized username retrieve at deployment time
+    const dbUsername = sm.Secret.fromSecretNameV2(this, 'knowledgeGraph-dbUsername', 'knowledgeGraph-dbUsername')
+
+    // Define the postgres database
+    this.dbInstance = new rds.DatabaseInstance(this, 'knowledgeGraph', {
+      vpc: vpcStack.vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      engine: rds.DatabaseInstanceEngine.postgres({
+        version: rds.PostgresEngineVersion.VER_13_4,
+      }),
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.BURSTABLE3,
+        ec2.InstanceSize.MICRO,
+      ),
+      credentials: rds.Credentials.fromUsername(dbUsername.secretValueFromJson("username").unsafeUnwrap() , {
+        secretName: this.secretPath
+      }),
+      multiAz: true,
+      allocatedStorage: 100,
+      maxAllocatedStorage: 200,
+      allowMajorVersionUpgrade: false,
+      autoMinorVersionUpgrade: true,
+      backupRetention: cdk.Duration.days(7),
+      deleteAutomatedBackups: true,
+      deletionProtection: true,
+      databaseName: 'knowledgeGraph',
+      publiclyAccessible: false,
+      cloudwatchLogsRetention: logs.RetentionDays.INFINITE,
+      storageEncrypted: true, // storage encryption at rest
+    });
+
+    this.dbInstance.connections.securityGroups.forEach(function (securityGroup) {
+      securityGroup.addIngressRule(ec2.Peer.ipv4('10.0.0.0/16'), ec2.Port.tcp(5432), 'Postgres Ingress');
+    });
+  }
+}
