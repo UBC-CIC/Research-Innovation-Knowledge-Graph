879c3c90c8574264ca40aa0659543a4df4afce59
Lance Holland <lanceholland01@gmail.com>
2023-03-28 15:26:40 -0700

    cdk first draft

---
+++ back_end/cdk/lib/glue-stack.ts
@@ -0,0 +1,240 @@
+import * as cdk from "aws-cdk-lib";
+import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import { VpcStack } from "./vpc-stack";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as s3n from "aws-cdk-lib/aws-s3-notifications";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as glue from "aws-cdk-lib/aws-glue";
+import * as sm from "aws-cdk-lib/aws-secretsmanager";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as logs from "aws-cdk-lib/aws-logs";
+import { triggers } from "aws-cdk-lib";
+import { DatabaseStack } from "./database-stack";
+import { Effect, ServicePrincipal } from "aws-cdk-lib/aws-iam";
+
+export class GlueStack extends Stack {
+
+  public readonly glueConnection: glue.CfnConnection;
+  public readonly glueConnectionName: string;
+  public readonly glueDmsConnection: glue.CfnConnection;
+  public readonly glueDmsConnectionName: string;
+  public readonly secretPath: string;
+  public readonly glueS3Bucket: s3.Bucket;
+  public readonly dmsTaskArn: string;
+
+  constructor(
+    scope: Construct,
+    id: string,
+    vpcStack: VpcStack,
+    databaseStack: DatabaseStack,
+    props?: StackProps
+  ) {
+    super(scope, id, props);
+    this.secretPath = databaseStack.secretPath;
+
+    // Create new Glue Role. DO NOT RENAME THE ROLE!!!
+    const roleName = "AWSGlueServiceRole-ShellJob";
+    const glueRole = new iam.Role(this, roleName, {
+      assumedBy: new iam.ServicePrincipal("glue.amazonaws.com"),
+      description: "Glue Service Role for Grant ETL",
+      roleName: roleName,
+    });
+
+    // Add different policies to glue-service-role
+    const glueServiceRolePolicy = iam.ManagedPolicy.fromAwsManagedPolicyName(
+      "service-role/AWSGlueServiceRole"
+    );
+    const glueConsoleFullAccessPolicy =
+      iam.ManagedPolicy.fromAwsManagedPolicyName("AWSGlueConsoleFullAccess");
+    const glueSecretManagerPolicy = iam.ManagedPolicy.fromAwsManagedPolicyName(
+      "SecretsManagerReadWrite"
+    );
+    const glueAmazonS3FullAccessPolicy =
+      iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonS3FullAccess");
+
+    glueRole.addManagedPolicy(glueServiceRolePolicy);
+    glueRole.addManagedPolicy(glueConsoleFullAccessPolicy);
+    glueRole.addManagedPolicy(glueSecretManagerPolicy);
+    glueRole.addManagedPolicy(glueAmazonS3FullAccessPolicy);
+
+    // Create S3 bucket for Glue Job scripts/data
+    this.glueS3Bucket = new s3.Bucket(this, "knowledgeGraph-glue-s3-bucket", {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      versioned: false,
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      serverAccessLogsPrefix: "accessLog"
+    });
+
+    // create S3 bucket for the grant data
+    const grantDataS3Bucket = new s3.Bucket(this, "knowledgeGraph-grant-data-s3-bucket", {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      versioned: false,
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      serverAccessLogsPrefix: "accessLog"
+    });
+
+    // Lambda function to trigger Glue jobs
+    const glueTrigger = new lambda.Function(this, "knowledgeGraph-s3-glue-trigger", {
+      runtime: lambda.Runtime.PYTHON_3_9,
+      functionName: "knowledgeGraph-s3-glue-trigger",
+      handler: "s3GlueTrigger.lambda_handler",
+      code: lambda.Code.fromAsset("lambda/s3-glue-trigger"),
+      timeout: cdk.Duration.minutes(1),
+      memorySize: 512,
+      vpc: vpcStack.vpc,
+      logRetention: logs.RetentionDays.SIX_MONTHS,
+    });
+
+    glueTrigger.addToRolePolicy(
+      new iam.PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: [
+          "glue:GetJob",
+          "glue:GetJobs",
+          "glue:GetJobRun",
+          "glue:GetJobRuns",
+          "glue:StartJobRun",
+          "glue:UpdateJob"
+        ],
+        resources: [
+          "*" // DO NOT CHANGE
+        ],
+      })
+    );
+
+    glueTrigger.addToRolePolicy(
+      new iam.PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: [
+          "s3:ListBucketVersions",
+          "s3:ListBucket",
+          "s3:ListObjectsV2",
+          "s3:ListMultipartUploadParts",
+          "s3:ListObjectVersions",
+        ],
+        resources: [
+          `arn:aws:s3:::${grantDataS3Bucket.bucketName}`,
+          `arn:aws:s3:::${grantDataS3Bucket.bucketName}/*`
+        ],
+      })
+    );
+
+    glueTrigger.addToRolePolicy(
+      new iam.PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: [
+          // CloudWatch Logs
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+        ],
+        resources: ["arn:aws:logs:*:*:*"],
+      })
+    );
+
+    glueTrigger.addToRolePolicy(new iam.PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: [
+        "dms:StartReplicationTask",
+        "dms:DescribeReplicationTasks"
+      ],
+      resources: ["*"] // Do Not change
+    }));  
+
+    // grant permission for s3 to invoke lambda
+    glueTrigger.addPermission("s3-invoke", {
+      principal: new iam.ServicePrincipal("s3.amazonaws.com"),
+      action: "lambda:InvokeFunction",
+      sourceAccount: this.account,
+      sourceArn: grantDataS3Bucket.bucketArn,
+    });
+
+    // Create a Connection to the PostgreSQL database inside the VPC
+    this.glueConnectionName = "postgres-conn";
+    const databaseSecret = sm.Secret.fromSecretNameV2(
+      this,
+      "databaseSecret",
+      databaseStack.secretPath
+    );
+    const host = databaseSecret.secretValueFromJson("host").unsafeUnwrap();
+    const dbname = databaseSecret.secretValueFromJson("dbname").unsafeUnwrap();
+    const connectionProperties: { [key: string]: any } = {
+      JDBC_ENFORCE_SSL: "true",
+      JDBC_CONNECTION_URL: "jdbc:postgresql://" + host + ":5432/" + dbname,
+      SKIP_CUSTOM_JDBC_CERT_VALIDATION: "true",
+      SECRET_ID: databaseStack.secretPath,
+      KAFKA_SSL_ENABLED: "false",
+    };
+    const publicSubnetId = vpcStack.vpc.publicSubnets[0].subnetId;
+    const securityGroup = vpcStack.vpc.vpcDefaultSecurityGroup;
+    this.glueConnection = new glue.CfnConnection(
+      this,
+      this.glueConnectionName,
+      {
+        catalogId: this.account, // this AWS account ID
+        connectionInput: {
+          name: this.glueConnectionName,
+          description: "a connection to the PostgreSQL database for Glue",
+          connectionType: "JDBC",
+          connectionProperties: connectionProperties,
+          physicalConnectionRequirements: {
+            availabilityZone: vpcStack.availabilityZones[0],
+            securityGroupIdList: [securityGroup],
+            subnetId: publicSubnetId,
+          },
+        },
+      }
+    );
+
+    // Glue Job: store data into table in database
+    const storeDataJobName = "knowledgeGraph-storeData";
+    const storeDataJob = new glue.CfnJob(this, storeDataJobName, {
+      name: storeDataJobName,
+      role: glueRole.roleArn,
+      command: {
+        name: "pythonshell",
+        pythonVersion: '3.9',
+        scriptLocation:
+          "s3://" +
+          this.glueS3Bucket.bucketName +
+          "/scripts/storeData" +
+          ".py",
+      },
+      executionProperty: {
+        maxConcurrentRuns: 1,
+      },
+      connections: {
+        connections: [this.glueConnectionName],
+      },
+      maxRetries: 0,
+      timeout: 2880, // 120 min timeout duration
+      glueVersion: '3.0',
+    });
+
+    // Deploy glue job to glue S3 bucket
+    new s3deploy.BucketDeployment(this, "DeployGlueJobFiles", {
+      sources: [s3deploy.Source.asset("./glue/scripts/")],
+      destinationBucket: this.glueS3Bucket,
+      destinationKeyPrefix: "scripts/",
+    });
+
+    // Grant S3 read/write role to Glue
+    this.glueS3Bucket.grantReadWrite(glueRole);
+    grantDataS3Bucket.grantReadWrite(glueRole);
+
+    // Destroy Glue related resources when GrantDataStack is deleted
+    storeDataJob.applyRemovalPolicy(RemovalPolicy.DESTROY);
+    glueTrigger.applyRemovalPolicy(RemovalPolicy.DESTROY);
+    this.glueConnection.applyRemovalPolicy(RemovalPolicy.DESTROY);
+    glueRole.applyRemovalPolicy(RemovalPolicy.DESTROY);
+  }
+}
