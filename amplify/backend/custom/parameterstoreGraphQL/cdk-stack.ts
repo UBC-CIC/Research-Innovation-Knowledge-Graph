2a06c38b2664e79d0a6316ab56ef0d905cc2581c
Lance Holland <lanceholland01@gmail.com>
2023-04-11 02:06:22 -0700

    fixing more package issues for amplify

--- amplify/backend/custom/parameterstoreGraphQL/cdk-stack.ts
+++ amplify/backend/custom/parameterstoreGraphQL/cdk-stack.ts
@@ -1,10 +1,11 @@
 import * as cdk from 'aws-cdk-lib'
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import {aws_ssm as ssm} from 'aws-cdk-lib'
 import * as AmplifyHelpers from '@aws-amplify/cli-extensibility-helper';
 import { AmplifyDependentResourcesAttributes } from '../../types/amplify-dependent-resources-ref';
 
-export class cdkStack extends cdk.Stack {
+export class cdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps, amplifyResourceProps?: AmplifyHelpers.AmplifyResourceProps) {
     super(scope, id, props);
     /* Do not remove - Amplify CLI automatically injects the current deployment environment in this input parameter */
